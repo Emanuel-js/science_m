d35951b37443f5aea77c75f16acd725a34b5594d
Amanuela996 <awol@qena.dev>
2024-12-20 23:40:55 +0300

    final

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "dist", // Output directory for the build
+    outDir: "dist",
+    rollupOptions: {
+      input: "src/main.tsx", // Entry point
+    },
   },
   base: "/",
   plugins: [react()],
